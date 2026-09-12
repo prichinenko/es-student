6cfa2ceeaf960c16a4fe49c145bf825b0c8ffd7e
dev4 <dev4@vitginiavip.ru>
2026-09-12 16:34:47 +0300

    1.3.1

---
+++ 131-hello-usb/main.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+const uint8_t LED_PIN = 25;
+const uint8_t BUTTON_PIN = 24;
+const uint8_t DEBUONCE_MS = 20;
+
+bool get_button_debounce(uint pin)
+{
+    bool state = gpio_get(pin);
+    sleep_ms(DEBUONCE_MS);
+    return state && gpio_get(pin);
+}
+
+int main()
+{
+    stdio_init_all();
+
+    // gpio_init(LED_PIN);
+    // gpio_set_dir(LED_PIN, GPIO_OUT);
+    // gpio_init(BUTTON_PIN);
+    // gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    // gpio_pull_up(BUTTON_PIN);
+
+    // bool led = false;
+    // bool previous = false;
+
+    // gpio_put(LED_PIN, led);
+
+    while (1)
+    {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+
+        // bool current = get_button_debounce(BUTTON_PIN);
+
+        // if (previous && !current)
+        // {
+        //     led = !led;
+        //     gpio_put(LED_PIN, led);
+        // }
+
+        // previous = current;
+    }
+
+    return 0;
+}
---
+++ 131-hello-usb/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+// #include "hardware/gpio.h"
+// #include "hardware/regs/addressmap.h"
+// #include "hardware/regs/sio.h"
